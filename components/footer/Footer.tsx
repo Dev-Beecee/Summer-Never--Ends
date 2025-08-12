2faf29024731f071a0616f8df2fcb8964e2a88b2
Nicolas-Brm <nicolas.boromee@gmail.com>
2025-08-12 11:31:07 -0400

    refactor: update Footer component by removing legal links and adjusting layout for improved clarity and design

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -14,7 +14,9 @@ export function Footer() {
     }, []);
 
     return (
-        <footer className="w-full border-t mt-12 py-6 text-center text-sm text-white">
+        <footer className="w-full border-t mt-12  text-center text-sm text-white">
+
+            <div className="bg-black text-white flex items-center justify-center py-[20px] px-[4px] mt-4 ">
             <a
                 href={reglementUrl || "/documents/reglement.pdf"}
                 target="_blank"
@@ -24,10 +26,6 @@ export function Footer() {
             >
                Voir le règlement du jeu
             </a>
-            <div className="bg-black text-white flex items-center justify-center py-[20px] px-[4px] mt-4 ">
-                <a href="/mentions-legales" className="hover:underline">Mentions légales</a>
-                <span className="mx-2">|</span>
-                <a href="/politique-confidentialite" className="hover:underline">Politique de confidentialité</a>
                 <span className="ml-4">
                   <svg width="22" height="21" viewBox="0 0 22 21" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M17.3537 0.0933838H4.45094C2.40346 0.0933838 0.743652 1.75319 0.743652 3.80067V16.7034C0.743652 18.7509 2.40346 20.4107 4.45094 20.4107H17.3537C19.4012 20.4107 21.061 18.7509 21.061 16.7034V3.80067C21.061 1.75319 19.4012 0.0933838 17.3537 0.0933838Z" fill="white"/>
